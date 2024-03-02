1b3fe5b2aa72d50b2cdfbde9e3909d1d52201fc6
Alan Kinsella <alankinsella1993@gmail.com>
2024-03-02 17:35:32 +0000

    Write transcribed audio to txt file in resources dir

--- modules/openai-transcribe.py
+++ modules/openai-transcribe.py
@@ -1,10 +1,22 @@
 from openai import OpenAI
-client = OpenAI()
+from colors import Colors
 
-audio_file= open("resources/harvard.wav", "rb")
+
+client = OpenAI()
+audio_source = "resources/harvard.wav"
+
+# GET AUDIO AND SEND TO OPENAI FOR TRANSCRIPTION
+print(Colors.YELLOW + "Transcribing audio..." + Colors.RESET)
+audio_file= open(audio_source, "rb")
 transcription = client.audio.transcriptions.create(
   model="whisper-1", 
   file=audio_file,
   response_format="text"
 )
 print(transcription)
+
+# WRITE TRANSCRIBED AUDIO TO FILE
+print(Colors.YELLOW + "Writing transcription to file..." + Colors.RESET)
+with open("resources/transcribed-audio.txt", "w") as file:
+    # Write text to the file
+    file.write(transcription)
